9aa02440409edd74c048658644c40a503bbd3a49
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-03 12:01:33 -0800

    Update EventListener.php

--- src/Terpz710/TokensAPI/EventListener.php
+++ src/Terpz710/TokensAPI/EventListener.php
@@ -11,13 +11,7 @@ use Terpz710\TokensAPI\API\TokenAPI;
 
 class EventListener implements Listener {
 
-  public function join(PlayerJoinEvent $event) :void{
-        $player = $event->getPlayer();
-        $api = TokenAPI::getInstance();
-    
-        if (!$api->getTokenBalance($player)) {
-            $initialTokenAmount = Tokens::getInstance()->getConfig()->get("starting_token_amount");
-            $api->setToken($player, $initialTokenAmount);
-        }
-    }
+  public function join(PlayerJoinEvent $event) : void{
+      TokenAPI::getInstance()->createBalance($event->getPlayer());
+  }
 }
